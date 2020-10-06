e7e8f3b40ca7b4a4fc0498534d18873d44d21bb4
Adrián Rodríguez <arodriguezpovea@gmail.com>
2020-10-06 12:51:23 +0200

    modificando ficheros

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,14 +11,53 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'es'
 
+
+#Carpeta donde se genera la web
+OUTPUT_PATH = '../blog-web/'
+
+#Carpeta del tema elegido
+THEME = './blueidea'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-OUTPUT_PATH = '../blog-web/'
 
+
+#Paginación:
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+#Configuración del Tema:
+
+# Display pages list on the top menu
+DISPLAY_PAGES_ON_MENU (True)
+
+# Display categories list on the top menu
+DISPLAY_CATEGORIES_ON_MENU (True)
+
+# Display categories list as a submenu of the top menu
+DISPLAY_CATEGORIES_ON_SUBMENU (False)
+
+# Display the category in the article's info
+DISPLAY_CATEGORIES_ON_POSTINFO (False)
+
+# Display the author in the article's info
+DISPLAY_AUTHOR_ON_POSTINFO (False)
+
+# Display the search form
+DISPLAY_SEARCH_FORM (False)
+
+# Sort pages list by a given attribute
+PAGES_SORT_ATTRIBUTE (Title)
+
+# Display the "Fork me on Github" banner
+GITHUB_URL (None)
 
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
@@ -29,8 +68,3 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
