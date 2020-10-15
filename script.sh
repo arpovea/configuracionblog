df14b6a847340b92524e0afe81bf6c3ef005ed59
Adrián Rodríguez <arodriguezpovea@gmail.com>
2020-10-15 14:20:33 +0200

    Agregando y modificando informacion

--- script.sh
+++ script.sh
@@ -1,11 +1,14 @@
 #! /usr/bin/bash 
 
 #Guardo cambios en el repositorio de configuración:
-
 git add.
 git commit -am "Agregando y modificando informacion"
 git push
+
+#Genero el sitio:
 pelican content -s pelicanconf.py
+
+#Me cambio al repositorio en el que esta el blog y aplico cambios:
 cd ../blog-web/
 git add .
 git commit -am "Agregando y modificando informacion"
